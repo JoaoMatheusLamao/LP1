de71937f7c395afd043c584b5256e7c93410ccda
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-08-10 19:53:28 -0300

    feat: ex1

---
+++ ex1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 50;
+        int soma = a+b;
+        System.out.println("Soma = " + soma);
+    }
+}

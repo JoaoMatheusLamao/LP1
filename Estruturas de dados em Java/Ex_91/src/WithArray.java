6261ab736e1f5fb17ef617ac268caebaf2d23f73
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-09-14 21:54:57 -0300

    Inserção dos exercicios referentes a tarefa "Estrutura de dados em Java"

---
+++ Estruturas de dados em Java/Ex_91/src/WithArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class WithArray {
+    public static void main(String[] args) {
+
+        int size_vet = 25;
+
+        double[] vet = new double[size_vet];
+
+        for (int i = 0; i <= size_vet -1; i++){
+            vet[i] = (new Random().nextInt(25));
+        }
+
+        System.out.println("\n" + Arrays.toString(vet) + "\n");
+
+        for (int i = 0; i < size_vet; i++) {
+            for (int j = 0; j < size_vet; j++) {
+                if (vet[i] == vet[j] && i < j ) {
+                    System.out.println("DUPLICATA: Índices " + i + " ("+ vet[i] + ")" +" e " + j + " ("+ vet[i] + ")" +" estão duplicados.");
+                }
+            }
+        }
+    }
+}
